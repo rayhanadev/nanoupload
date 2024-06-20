568542662dd1a8be1012b1b9422e8ebb37668afd
ray <me@rayhanadev.com>
2024-06-20 18:44:40 -0500

    feat: implement clipboard checking

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use copypasta::{ClipboardContext, ClipboardProvider};
+use std::path::Path;
 use tauri::GlobalShortcutManager;
-use tauri::Manager;
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use url::Url;
 
 fn main() {
     let about = CustomMenuItem::new("about".to_string(), "About");
-    let check_for_updates = CustomMenuItem::new("check_for_updates".to_string(), "Check for Updates");
+    let check_for_updates =
+        CustomMenuItem::new("check_for_updates".to_string(), "Check for Updates");
     let settings = CustomMenuItem::new("settings".to_string(), "Settings");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let tray_menu = SystemTrayMenu::new()
@@ -28,7 +25,24 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             if let Err(e) = app.global_shortcut_manager().register("Cmd+Shift+U", || {
-                println!("hello world");
+                let mut ctx = ClipboardContext::new().unwrap();
+                let clipboard_content = ctx.get_contents().unwrap_or_else(|_| String::new());
+
+                if clipboard_content.is_empty() {
+                    println!("Clipboard is empty.");
+                } else {
+                    println!("Clipboard content: {}", clipboard_content);
+
+                    if is_image(&clipboard_content) {
+                        println!("Clipboard contains an image.");
+                    } else if is_url(&clipboard_content) {
+                        println!("Clipboard contains a URL.");
+                    } else if is_file_path(&clipboard_content) {
+                        println!("Clipboard contains a file path.");
+                    } else {
+                        println!("Clipboard contains text.");
+                    }
+                }
             }) {
                 println!("global shortcut register failed: {}", e);
             };
@@ -36,7 +50,7 @@ fn main() {
             Ok(())
         })
         .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
+        .on_system_tray_event(|_app, event| match event {
             SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
                 "about" => {
                     // TODO: open browser to website
@@ -54,7 +68,6 @@ fn main() {
             },
             _ => {}
         })
-        .invoke_handler(tauri::generate_handler![greet])
         .build(tauri::generate_context!())
         .expect("error while building tauri application")
         .run(|_app_handle, event| match event {
@@ -63,4 +76,17 @@ fn main() {
             }
             _ => {}
         });
+}
+
+fn is_image(content: &str) -> bool {
+    // TODO: research this check
+    false
+}
+
+fn is_url(content: &str) -> bool {
+    Url::parse(content).is_ok()
+}
+
+fn is_file_path(content: &str) -> bool {
+    Path::new(content).exists()
 }
