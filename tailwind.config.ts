78e75c2a646a7585436b7663ad8718cc8a3dba86
ray <me@rayhanadev.com>
2024-06-20 14:06:55 -0500

    refactor: use typescript config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -198,4 +199,4 @@ export default {
     },
   },
   plugins: [],
-};
+} satisfies Config;
