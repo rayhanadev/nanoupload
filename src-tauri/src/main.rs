2b10690385edbb5dbdf08f894509ff2e3b187b22
ray <me@rayhanadev.com>
2024-06-20 17:23:44 -0500

    feat: add background app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,66 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::GlobalShortcutManager;
+use tauri::Manager;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 fn main() {
+    let about = CustomMenuItem::new("about".to_string(), "About");
+    let check_for_updates = CustomMenuItem::new("check_for_updates".to_string(), "Check for Updates");
+    let settings = CustomMenuItem::new("settings".to_string(), "Settings");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(about)
+        .add_item(check_for_updates)
+        .add_item(settings)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(|app| {
+            if let Err(e) = app.global_shortcut_manager().register("Cmd+Shift+U", || {
+                println!("hello world");
+            }) {
+                println!("global shortcut register failed: {}", e);
+            };
+
+            Ok(())
+        })
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "about" => {
+                    // TODO: open browser to website
+                }
+                "check_for_updates" => {
+                    // TODO: check for updates
+                }
+                "settings" => {
+                    // TODO: open settings
+                }
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
