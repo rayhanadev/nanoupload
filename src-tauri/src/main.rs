be590e6bfe464616a06d470d9e4c2182fde2ef02
ray <me@rayhanadev.com>
2024-06-20 21:43:10 -0500

    feat: add notifications

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,10 @@
 
 use arboard::Clipboard;
 use std::path::Path;
-use tauri::GlobalShortcutManager;
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::api::notification::Notification;
+use tauri::{AppHandle, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{GlobalShortcutManager, WindowEvent};
+use tauri::async_runtime::{self};
 use url::Url;
 
 fn main() {
@@ -24,42 +26,22 @@ fn main() {
 
     tauri::Builder::default()
         .setup(|app| {
-            if let Err(e) = app.global_shortcut_manager().register("Cmd+Shift+U", || {
-                // attempt to get an image from the clipboard and if that is not possible attempt to get text
-                let mut clipboard = Clipboard::new().unwrap();
+            let (tx, mut rx) = async_runtime::channel::<()>(1);
+            
+            let handle = app.handle().clone();
 
-                let clipboard_image_result = clipboard.get_image();
-
-                if clipboard_image_result.is_ok() {
-                    let _clipboard_image = clipboard_image_result.unwrap();
-                    println!("clipboard content is an image");
-                } else {
-                    let clipboard_text_result = clipboard.get_text();
-                    if clipboard_text_result.is_ok() {
-                        let clipboard_text = clipboard_text_result.unwrap();
-
-                        if is_url(&clipboard_text) {
-                            println!("clipboard content is a url")
-                        }
-                        
-                        else if is_file_path(&clipboard_text) {
-                            println!("clipboard content is a file path")
-                        }
-
-                        else if clipboard_text.len() > 0 {
-                            println!("clipboard content is text")
-                        }
-
-                        else {
-                            println!("clipboard is empty")
-                        }
-
-                        println!("clipboard text: {:?}", clipboard_text);
-                    }
+            async_runtime::spawn(async move {
+                while rx.recv().await.is_some() {
+                    handle_clipboard(&handle).await;
                 }
-            }) {
-                println!("global shortcut register failed: {}", e);
-            };
+            });
+
+            let mut global_shortcut_manager = app.global_shortcut_manager();
+            global_shortcut_manager
+                .register("Cmd+Shift+U", move || {
+                    let _ = tx.try_send(());
+                })
+                .expect("Failed to register global shortcut");
 
             Ok(())
         })
@@ -82,14 +64,45 @@ fn main() {
             },
             _ => {}
         })
-        .build(tauri::generate_context!())
-        .expect("error while building tauri application")
-        .run(|_app_handle, event| match event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
+        .on_window_event(|event| {
+            if let WindowEvent::CloseRequested { api, .. } = event.event() {
+                let window = event.window().clone();
+                api.prevent_close();
+                window.hide().unwrap();
             }
-            _ => {}
-        });
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+async fn handle_clipboard(app: &AppHandle) {
+    let mut clipboard = Clipboard::new().unwrap();
+
+    let clipboard_image_result = clipboard.get_image();
+
+    if clipboard_image_result.is_ok() {
+        let _clipboard_image = clipboard_image_result.unwrap();
+        println!("clipboard content is an image");
+    } else {
+        let clipboard_text_result = clipboard.get_text();
+        if clipboard_text_result.is_ok() {
+            let clipboard_text = clipboard_text_result.unwrap();
+
+            if is_url(&clipboard_text) {
+                println!("clipboard content is a url")
+            } else if is_file_path(&clipboard_text) {
+                println!("clipboard content is a file path")
+            } else if clipboard_text.len() > 0 {
+                println!("clipboard content is text")
+            } else {
+                println!("clipboard is empty")
+            }
+
+            println!("clipboard text: {:?}", clipboard_text);
+        }
+    }
+
+    show_notification(app, "Clipboard", "Clipboard content has been processed");
 }
 
 fn is_url(content: &str) -> bool {
@@ -98,4 +111,12 @@ fn is_url(content: &str) -> bool {
 
 fn is_file_path(content: &str) -> bool {
     Path::new(content).exists()
+}
+
+fn show_notification(app: &AppHandle, title: &str, message: &str) {
+    Notification::new(&app.config().tauri.bundle.identifier)
+        .title(title)
+        .body(message)
+        .show()
+        .unwrap();
 }
