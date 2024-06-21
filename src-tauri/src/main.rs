05c5e5fa94fb21b4f4a81418a4d76b1eba8acfe5
ray <me@rayhanadev.com>
2024-06-20 19:34:42 -0500

    refactor: use janky arboard setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use copypasta::{ClipboardContext, ClipboardProvider};
+use arboard::Clipboard;
 use std::path::Path;
 use tauri::GlobalShortcutManager;
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
@@ -25,22 +25,36 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             if let Err(e) = app.global_shortcut_manager().register("Cmd+Shift+U", || {
-                let mut ctx = ClipboardContext::new().unwrap();
-                let clipboard_content = ctx.get_contents().unwrap_or_else(|_| String::new());
+                // attempt to get an image from the clipboard and if that is not possible attempt to get text
+                let mut clipboard = Clipboard::new().unwrap();
 
-                if clipboard_content.is_empty() {
-                    println!("Clipboard is empty.");
+                let clipboard_image_result = clipboard.get_image();
+
+                if clipboard_image_result.is_ok() {
+                    let _clipboard_image = clipboard_image_result.unwrap();
+                    println!("clipboard content is an image");
                 } else {
-                    println!("Clipboard content: {}", clipboard_content);
+                    let clipboard_text_result = clipboard.get_text();
+                    if clipboard_text_result.is_ok() {
+                        let clipboard_text = clipboard_text_result.unwrap();
 
-                    if is_image(&clipboard_content) {
-                        println!("Clipboard contains an image.");
-                    } else if is_url(&clipboard_content) {
-                        println!("Clipboard contains a URL.");
-                    } else if is_file_path(&clipboard_content) {
-                        println!("Clipboard contains a file path.");
-                    } else {
-                        println!("Clipboard contains text.");
+                        if is_url(&clipboard_text) {
+                            println!("clipboard content is a url")
+                        }
+                        
+                        else if is_file_path(&clipboard_text) {
+                            println!("clipboard content is a file path")
+                        }
+
+                        else if clipboard_text.len() > 0 {
+                            println!("clipboard content is text")
+                        }
+
+                        else {
+                            println!("clipboard is empty")
+                        }
+
+                        println!("clipboard text: {:?}", clipboard_text);
                     }
                 }
             }) {
@@ -76,11 +90,6 @@ fn main() {
             }
             _ => {}
         });
-}
-
-fn is_image(content: &str) -> bool {
-    // TODO: research this check
-    false
 }
 
 fn is_url(content: &str) -> bool {
